555ac8a165340e0efb40dead61ade6f37d1c7bb9
Paulo <plsergioal@gmail.com>
2019-08-29 10:38:20 -0300

    crud - read-create

--- meuPrimeiroProjeto/clientes/urls.py
+++ meuPrimeiroProjeto/clientes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import persons_list
+from .views import persons_list, persons_new
 
 urlpatterns = [
-    path('list/', persons_list),
+    path('list/', persons_list, name="person_list"),
+    path('new/', persons_new, name="person_new"),
 ]
--- meuPrimeiroProjeto/clientes/views.py
+++ meuPrimeiroProjeto/clientes/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Person
+from .forms import PersonForm
 
 def persons_list(request):
-    return render(request, 'pessoa.html')
+    persons = Person.objects.all()
+    return render(request, 'person.html', {'persons': persons})
+
+def persons_new(request):
+    form = PersonForm(request.POST, None)
+    return render(request, 'person_form.html',{'form':form})

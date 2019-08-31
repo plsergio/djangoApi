2c9f15561e8a024be20298f503e2081dec822841
Paulo <plsergioal@gmail.com>
2019-08-30 23:30:53 -0300

    crud completo

--- meuPrimeiroProjeto/clientes/urls.py
+++ meuPrimeiroProjeto/clientes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import persons_list, persons_new
+from .views import persons_list, persons_new,person_update,person_delete
 
 urlpatterns = [
     path('list/', persons_list, name="person_list"),
     path('new/', persons_new, name="person_new"),
+    path('update/<int:id>/', person_update, name="person_update"),
+    path('delete/<int:id>/', person_delete, name="person_delete"),
 ]
